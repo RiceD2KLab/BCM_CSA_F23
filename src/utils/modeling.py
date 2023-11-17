dc6dc916af740dc7cd19cef9238bebc18f9b04b2
Huailin Tang <huailintang@yahoo.com>
2023-11-17 07:38:16 -0600

    update modeling module and table visualization

--- src/utils/modeling.py
+++ src/utils/modeling.py
@@ -8,6 +8,7 @@ from sklearn.ensemble import RandomForestRegressor
 from sklearn.linear_model import LinearRegression, Ridge, Lasso
 from sklearn.tree import DecisionTreeRegressor
 
+
 def split_data(data, features, target):
     """Split on test/train/val.
 
@@ -20,9 +21,13 @@ def split_data(data, features, target):
     """
     # Split on test/train 80/20
     # split startified on target
-    X_train, X_test, y_train, y_test = train_test_split(data[features], data[target], test_size=0.3, random_state=1)
+    X_train, X_test, y_train, y_test = train_test_split(
+        data[features], data[target], test_size=0.3, random_state=1
+    )
     # make validation set
-    X_train, X_val, y_train, y_val = train_test_split(X_train, y_train, test_size=0.2, random_state=1)
+    X_train, X_val, y_train, y_val = train_test_split(
+        X_train, y_train, test_size=0.2, random_state=1
+    )
     return X_train, X_test, y_train, y_test, X_val, y_val
 
 
@@ -37,11 +42,12 @@ def train_model(model, X_train, y_train, X_test, y_test, X_val, y_val):
         y_test (pd.DataFrame): test labels
         X_val (pd.DataFrame): validation data
         y_val (pd.DataFrame): validation labels
-    
+
     Returns:
         tuple: mae, model
     """
     from sklearn.metrics import mean_absolute_error
+
     model.fit(X_train, y_train)
     y_pred = model.predict(X_val)
     # Evaluaate
@@ -63,69 +69,146 @@ def find_best_data(folder_loc, datasets, target):
     best_dataset = None
 
     for dataset in tqdm(datasets):
-    # Load data
+        # Load data
         df = pd.read_csv(folder_loc + dataset)
 
         features = df.columns.tolist()
         features.remove(target)
 
         # Split data
-        X_train, X_test, y_train, y_test, X_val, y_val = split_data(df, features, target)
+        X_train, X_test, y_train, y_test, X_val, y_val = split_data(
+            df, features, target
+        )
 
         # Train xgboost
 
-        mae_xgb, model_xgb = train_model(XGBRegressor(random_state=1), X_train, y_train, X_test, y_test, X_val, y_val)
+        mae_xgb, model_xgb = train_model(
+            XGBRegressor(random_state=1), X_train, y_train, X_test, y_test, X_val, y_val
+        )
 
         # Train random forest
 
-        mae_rf, model_rf = train_model(RandomForestRegressor(random_state=1), X_train, y_train, X_test, y_test, X_val, y_val)
+        mae_rf, model_rf = train_model(
+            RandomForestRegressor(random_state=1),
+            X_train,
+            y_train,
+            X_test,
+            y_test,
+            X_val,
+            y_val,
+        )
 
         # Train linear regression
 
-        mae_lr, model_lr = train_model(LinearRegression(), X_train, y_train, X_test, y_test, X_val, y_val)
+        mae_lr, model_lr = train_model(
+            LinearRegression(), X_train, y_train, X_test, y_test, X_val, y_val
+        )
 
         # Train lasso
 
-        mae_lasso, model_lasso = train_model(Lasso(random_state=1), X_train, y_train, X_test, y_test, X_val, y_val)
+        mae_lasso, model_lasso = train_model(
+            Lasso(random_state=1), X_train, y_train, X_test, y_test, X_val, y_val
+        )
 
         # Train ridge
 
-        mae_ridge, model_ridge = train_model(Ridge(random_state=1), X_train, y_train, X_test, y_test, X_val, y_val)
+        mae_ridge, model_ridge = train_model(
+            Ridge(random_state=1), X_train, y_train, X_test, y_test, X_val, y_val
+        )
 
         # Train decision tree
 
-        mae_dt, model_dt = train_model(DecisionTreeRegressor(random_state=1), X_train, y_train, X_test, y_test, X_val, y_val)
+        mae_dt, model_dt = train_model(
+            DecisionTreeRegressor(random_state=1),
+            X_train,
+            y_train,
+            X_test,
+            y_test,
+            X_val,
+            y_val,
+        )
 
         # Save best model
 
         if mae_xgb < best_mae:
             best_mae = mae_xgb
             best_model = model_xgb
-            model_name = 'xgb'
+            model_name = "xgb"
             best_dataset = dataset
         if mae_rf < best_mae:
             best_mae = mae_rf
             best_model = model_rf
-            model_name = 'rf'
+            model_name = "rf"
             best_dataset = dataset
         if mae_lr < best_mae:
             best_mae = mae_lr
             best_model = model_lr
-            model_name = 'lr'
+            model_name = "lr"
             best_dataset = dataset
         if mae_lasso < best_mae:
             best_mae = mae_lasso
             best_model = model_lasso
-            model_name = 'lasso'
+            model_name = "lasso"
             best_dataset = dataset
         if mae_ridge < best_mae:
             best_mae = mae_ridge
             best_model = model_ridge
-            model_name = 'ridge'
+            model_name = "ridge"
             best_dataset = dataset
         if mae_dt < best_mae:
             best_mae = mae_dt
             best_model = model_dt
-            model_name = 'dt'
+            model_name = "dt"
             best_dataset = dataset
     return best_mae, best_model, model_name, best_dataset
+
+
+def find_best_data_each_model(folder_loc, datasets, target):
+    """GIven datasets and target variable, return the best dataset for each model
+
+    Args:
+        folder_loc (string): location of folder contianing datasets
+        datasets (array): list of dataset locations within folder
+        target (string): target variable
+    """
+    best_models = {
+        "xgb": {"mae": 100000, "model": None, "dataset": None},
+        "rf": {"mae": 100000, "model": None, "dataset": None},
+        "lr": {"mae": 100000, "model": None, "dataset": None},
+        "lasso": {"mae": 100000, "model": None, "dataset": None},
+        "ridge": {"mae": 100000, "model": None, "dataset": None},
+        "dt": {"mae": 100000, "model": None, "dataset": None},
+    }
+
+    for dataset in tqdm(datasets):
+        # Load data
+        df = pd.read_csv(folder_loc + dataset)
+        features = df.columns.tolist()
+        features.remove(target)
+
+        # Split data
+        X_train, X_test, y_train, y_test, X_val, y_val = split_data(
+            df, features, target
+        )
+
+        # Define a dictionary to store models and their training function
+        models = {
+            "xgb": XGBRegressor(random_state=1),
+            "rf": RandomForestRegressor(random_state=1),
+            "lr": LinearRegression(),
+            "lasso": Lasso(random_state=1),
+            "ridge": Ridge(random_state=1),
+            "dt": DecisionTreeRegressor(random_state=1),
+        }
+
+        # Train and evaluate each model
+        for name, model in models.items():
+            mae, trained_model = train_model(
+                model, X_train, y_train, X_test, y_test, X_val, y_val
+            )
+
+            if mae < best_models[name]["mae"]:
+                best_models[name]["mae"] = mae
+                best_models[name]["model"] = trained_model
+                best_models[name]["dataset"] = dataset
+    return best_models
--- src/utils/visualization.py
+++ src/utils/visualization.py
@@ -1,4 +1,6 @@
 import numpy as np
+import pandas as pd
+
 
 # Common function to plot histogram and median
 def plot_histogram(axis, data_s1, title, xlabel, data_s2=None):
@@ -17,25 +19,49 @@ def plot_histogram(axis, data_s1, title, xlabel, data_s2=None):
     # Plotting for the first dataset (shhs1)
     median_s1 = data_s1.median()
     right_amt = 1.1
-    axis.hist(data_s1, bins=25, alpha=1, edgecolor='black', color='steelblue', label='shhs1')
-    axis.axvline(median_s1, color='red', linestyle='-', label='Median (shhs1)')
-    axis.text(median_s1 * right_amt, axis.get_ylim()[1] * 0.95, f'Median (shhs1): {median_s1:.2f}', color='red', ha='left', fontsize=10)
-    
+    axis.hist(
+        data_s1, bins=25, alpha=1, edgecolor="black", color="steelblue", label="shhs1"
+    )
+    axis.axvline(median_s1, color="red", linestyle="-", label="Median (shhs1)")
+    axis.text(
+        median_s1 * right_amt,
+        axis.get_ylim()[1] * 0.95,
+        f"Median (shhs1): {median_s1:.2f}",
+        color="red",
+        ha="left",
+        fontsize=10,
+    )
+
     # Plotting for the second dataset (shhs2) if provided
     if data_s2 is not None:
         median_s2 = data_s2.median()
-        axis.hist(data_s2, bins=25, alpha=0.7, edgecolor='black', color='lightsteelblue', label='shhs2')
-        axis.axvline(median_s2, color='red', linestyle=':', label='Median (shhs2)')
-        axis.text(median_s2 * right_amt, axis.get_ylim()[1] * 0.91, f'Median (shhs1): {median_s2:.2f}', color='red', ha='left', fontsize=10)
+        axis.hist(
+            data_s2,
+            bins=25,
+            alpha=0.7,
+            edgecolor="black",
+            color="lightsteelblue",
+            label="shhs2",
+        )
+        axis.axvline(median_s2, color="red", linestyle=":", label="Median (shhs2)")
+        axis.text(
+            median_s2 * right_amt,
+            axis.get_ylim()[1] * 0.91,
+            f"Median (shhs1): {median_s2:.2f}",
+            color="red",
+            ha="left",
+            fontsize=10,
+        )
 
     axis.set_title(title, fontsize=20)
     axis.set_xlabel(xlabel, fontsize=18)
-    axis.set_ylabel('Number of Participants', fontsize=18)  # Set Y-axis label
-    axis.tick_params(axis='both', labelsize=14)  # Set tick label font size
+    axis.set_ylabel("Number of Participants", fontsize=18)  # Set Y-axis label
+    axis.tick_params(axis="both", labelsize=14)  # Set tick label font size
+
 
 def plot_bar(ax, data_col, title, bins, bin_labels, colors):
     """Plots a sub-bar graph given a dataframe column and an aesthetics configuration
-    
+
     Args:
         ax: subplot to draw on
         data_col (list): column of data that contains frequency data
@@ -43,22 +69,79 @@ def plot_bar(ax, data_col, title, bins, bin_labels, colors):
         bins ([int])
         bin_labels ([string])
         colors ([string])
-    
+
     Returns:
         None
     """
     counts, _ = np.histogram(data_col, bins=bins)
     bars = ax.bar(bin_labels, counts, color=colors)
 
-    ax.set_xlabel('Events per Hour', fontsize=18)
-    ax.set_ylabel('Number of Participants', fontsize=18)
+    ax.set_xlabel("Events per Hour", fontsize=18)
+    ax.set_ylabel("Number of Participants", fontsize=18)
     ax.set_title(title, fontsize=20)
 
     # Adjust tick font sizes
-    ax.tick_params(axis='both', labelsize=14)
+    ax.tick_params(axis="both", labelsize=14)
 
     # Annotate each bar with its count
     for bar in bars:
         height = bar.get_height()
-        ax.text(bar.get_x() + bar.get_width() / 2, height - 10, str(int(height)),
-                ha='center', va='bottom', fontsize=14)
+        ax.text(
+            bar.get_x() + bar.get_width() / 2,
+            height - 10,
+            str(int(height)),
+            ha="center",
+            va="bottom",
+            fontsize=14,
+        )
+
+
+def feature_selection_cheap_feature_comparison(cheap_features, feature_selection):
+    # Convert dictionaries to dataframes
+    df_cheap_features = pd.DataFrame(cheap_features).T.reset_index()[
+        ["index", "mae", "dataset"]
+    ]
+    df_feature_selection = pd.DataFrame(feature_selection).T.reset_index()[
+        ["index", "mae", "dataset"]
+    ]
+
+    # Rename columns for clarity
+    df_cheap_features.columns = [
+        "Model",
+        "MAE for Cheap Features",
+        "Dataset for Cheap Features",
+    ]
+    df_feature_selection.columns = [
+        "Model",
+        "MAE for Feature Selection",
+        "Dataset for Feature Selection",
+    ]
+
+    # Merge dataframes on model names
+    comparison_df = pd.merge(df_cheap_features, df_feature_selection, on="Model")
+
+    comparison_df["Model"] = comparison_df["Model"].replace(
+        {
+            "xgb": "XGBoost",
+            "rf": "random forest",
+            "lr": "linear regression",
+            "lasso": "lasso regression",
+            "ridge": "ridge regression",
+            "dt": "decision tree",
+        }
+    )
+
+    # Use Pandas styling to create a table visualization and highlight the lowest MAE for Feature Selection and MAE for Cheap Features
+    styled_table = comparison_df.style.set_table_styles(
+        [
+            {
+                "selector": "th",
+                "props": [("font-size", "12pt"), ("text-align", "center")],
+            },
+            {"selector": "td", "props": [("text-align", "center")]},
+        ]
+    ).highlight_min(
+        subset=["MAE for Cheap Features", "MAE for Feature Selection"], color="red"
+    )
+
+    return styled_table
